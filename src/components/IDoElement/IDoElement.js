2d426757d9ff231855ea4958a428e400f41fb357
jorge <91345902+jreyesgfi@users.noreply.github.com>
2022-10-22 19:03:15 +0200

    initial animation card

--- src/components/IDoElement/IDoElement.js
+++ src/components/IDoElement/IDoElement.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from 'react';
-import { motion, AnimatePresence, useMotionValue, useTransform } from "framer-motion";
+import React, { useEffect, useRef, useState } from 'react';
+import { motion, AnimatePresence, useMotionValue, useTransform, useAnimation } from "framer-motion";
 import { Heading, Image, Text } from '../../globalStyles'
 import { HolderToAnimate, IDoDescription, IDoElementImage, IDoElementImageHolder, IDoElementImagesWrapper, IDoElementWrapper, IDoTextArea } from './IDoElementStyles'
+import {useInView} from 'react-intersection-observer';
 
 const IDoElement = ({ data }) => {
     const [clicked, setClicked] = useState(false);
@@ -27,12 +28,28 @@ const IDoElement = ({ data }) => {
     const y = useMotionValue(0);
     const rotateX = useTransform(y, [-100, 100], [30, -30]);
     const rotateY = useTransform(x, [-100, 100], [-30, 30]);
-    const cardRef = useRef();
+    const {ref:cardRef,inView} = useInView({threshold:0.7});
+    // animation params
+    const initial =(index)=> ({y: 20+2*index,opacity: 0});
+    const transition =(index)=> ({delay:0.2*index, duration:0.5+0.1*index});
+    const animation = useAnimation();
+    useEffect(()=> {
+        console.log(inView)
+        console.log(cardRef)
+        if(inView){
+            animation.start({
+                opacity:1,
+                y:0
+            })
+        }
+    },[inView,animation]);
+
 
     return (
         <IDoElementWrapper
             onClick={() => { setClicked(!clicked) }}
             as={motion.div}
+            ref={node=>{cardRef(node);}}
             whileHover={"hover"}
             whileTap={"tap"}
             variants={cardVariants}>
@@ -46,11 +63,15 @@ const IDoElement = ({ data }) => {
                     {data?.images?.map((image, index) => (
                         <IDoElementImageHolder
                             as={motion.div}
+                            key={index}
                             drag
                             dragConstraints={{left:0,right:0,top:0,bottom:0}}
                             dragElastic={0.1}
                             whileDrag={{ translateX:-10, scale: 1.2 }}
-                            style={{x,y,rotateX,rotateY,z:index===0?500:800}}>
+                            style={{x,y,rotateX,rotateY,z:index===0?500:800}}
+                            initial={initial(index)}
+                            animate={animation}
+                            transition={transition(index)}>
                             <IDoElementImage src={image}/>
                         </IDoElementImageHolder>
                     ))}
